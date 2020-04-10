093c27e1075faa93fee3580fffba217c886f66df
rinaykumar <rinaykumar@gmail.com>
2020-04-09 19:44:37 -0700

    QA lab

--- qa/src/main/java/Main.java
+++ qa/src/main/java/Main.java
@@ -22,8 +22,40 @@ public class Main {
     Assert.assertEquals(isEven(8), true);
   }
 
+  @Test
+  public void testExtractGetParamValueFound() {
+    Assert.assertEquals(extractGetParam("http://localhost:1234?abc=123", "abc"),
+            "123");
+  }
+
+  @Test
+  public void testExtractGetParamMissingKey(){
+    Assert.assertEquals(extractGetParam("http://localhost:1234?abcd=123", "abc"),
+            null);
+  }
+
+  @Test
+  public void testMultipleParams(){
+    Assert.assertEquals(extractGetParam("http://localhost:1234?abc=123&b=4", "abc"),
+            "123");
+  }
+
   public static boolean isEven(int j){
-    return false;
+    return j % 2 == 0;
+  }
+
+  public static String extractGetParam(String url, String key) {
+    String[] parts = url.split(":");
+    System.out.println(parts[2]);
+    String[] portParts = parts[2].split("\\?");
+    System.out.println(portParts[1]);
+    String[] keyValueParts = portParts[1].split("=");
+
+    if (keyValueParts[0].equals(key)){
+      return keyValueParts[1];
+    }
+
+    return null;
   }
 
 }
