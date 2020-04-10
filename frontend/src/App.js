05d0c579ca16568b83d43f8b8353df1524433fd4
rinaykumar <rinaykumar@gmail.com>
2020-04-09 21:01:35 -0700

    qa/ws frontend lab

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+  import React from 'react';
 import './App.css';
+
+const ws = new WebSocket('ws://localhost:1235/ws');
 
 const App = ({ }) => {
   const [totalUsers, setTotalUsers] = React.useState(0);
-  const [notes, setNotes] = React.useState(['test']);
+  const [notes, setNotes] = React.useState(['test', 'hello', 'hello2']);
   const [note, setNote] = React.useState('');
+
+  React.useEffect(() => {
+    ws.addEventListener('message', (stringMessage) => {
+      console.log(stringMessage.data); // From server
+      const newNotes = notes;
+      newNotes.push(stringMessage.data);
+      setNotes(newNotes);
+      console.log(newNotes);
+    });
+  }, []);
 
   const handleSubmit = () => {
     console.log(note);
+    ws.send(note);
     setNote('');
+  };
+
+  const deleteFunction = (i) => {
+    console.log('Should delete item at index', i);
   };
 
   return (
@@ -25,6 +42,7 @@ const App = ({ }) => {
           <div className="note-item" key={i}>
             <div>{i}</div>
             <div>{note}</div>
+            <button onClick={() => deleteFunction(i)}>Delete</button>
           </div>
         ))}
       </div>
